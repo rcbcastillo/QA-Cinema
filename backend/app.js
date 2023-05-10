3362a7c9523f49127b031bb27ef335d4f6e90cb8
rcbcastillo <rcbcastillo@gmail.com>
2023-05-10 16:32:15 +0100

    Add app.use(/users, userRouter)

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const movieRouter = require("./routes/movieRoutes");
+const userRouter = require("./route/userRoutes");
 const { uri } = require("./atlas_uri");
 const movieModel = require("./models/movieModel");
+const userModel = require("./models/userModel");
 
 app.use(express.json());
 
@@ -20,6 +22,7 @@ mongoose
   });
 
 app.use("/movies", movieRouter);
+app.use("/users", userRouter);
 
 // Error handling
 app.use((err, req, res, next) => {

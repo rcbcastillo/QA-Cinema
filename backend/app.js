ee2833594b1bced114a8ef2aac7947349b26be02
rcbcastillo <rcbcastillo@gmail.com>
2023-05-11 12:17:54 +0100

    Add changes to app.js as connection to db and listening were moved to connection.js

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const movieRouter = require("./routes/movieRoutes");
-const userRouter = require("./route/userRoutes");
-const { uri } = require("./atlas_uri");
+const userRouter = require("./routes/userRoutes");
 const movieModel = require("./models/movieModel");
 const userModel = require("./models/userModel");
 
@@ -12,26 +11,12 @@ app.use(express.json());
 
 //app.use(cors());
 
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 app.use("/movies", movieRouter);
 app.use("/users", userRouter);
 
 // Error handling
 app.use((err, req, res, next) => {
   res.status(err.status).send(err.msg);
-});
-
-// Server listening
-app.listen(9090, () => {
-  console.log("server started on port 9090");
 });
 
 module.exports = app;

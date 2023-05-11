17ca301cd6734bebe63b561d0eb65e515a4aab38
rcbcastillo <rcbcastillo@gmail.com>
2023-05-11 12:19:39 +0100

    Make sure changes to package.json and imports are updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,23 @@ const express = require("express");
 const userModel = require("../models/userModel");
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res, next) => {
-  res.send("User Page, in progress..");
-});
-
 userRouter.get("/readUsers", async (req, res, next) => {
   try {
     const usersFound = await userModel.find();
-    res.json(usersFound);
+    res.status(200).json(usersFound);
+  } catch (err) {
+    return next({
+      status: 500,
+      msg: err.message,
+    });
+  }
+});
+
+userRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const readedUserById = await userModel.findById(userId);
+    res.status(200).json(readedUserById);
   } catch (err) {
     return next({
       status: 500,

2eebcb746a16dfde5f5ea158696fef6ff091c446
Helen Pickavance <helenpjobs@gmail.com>
2023-05-12 14:22:22 +0100

    Gallery & MoviePanel working with static JSON

--- qa-cinema/src/Components/Gallery.jsx
+++ qa-cinema/src/Components/Gallery.jsx
@@ -3,12 +3,30 @@ import MoviePanel from './MoviePanel';
 
 const jsonDataArray = [
     {
-        img: '',
-        title: '',
-        rating: '',
-        year: '',
-        plot: '',
-    }
+        'id': '12223',
+        'imgSrc': 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+        'title': 'Avengers Assemble',
+        'rating': '12A',
+        'year': 2012,
+        'length': 143,
+        'actors': 'Robert Downey Jnr, Chris Evans',
+        'plot': `Can the world's most powerful team of Super Heroes put aside their differences and learn to 
+                live together under one roof long enough to save the world from ultimate destruction? The answer 
+                will be revealed when the Red Skull and M.O.D.O.K. wage war on The Avengers.`
+    },
+    {
+        'id': '22234',
+        'imgSrc': 'https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+        'title': 'Deadpool',
+        'rating': '15',
+        'year': '2016',
+        'length': 108,
+        'actors': 'Ryan Reynolds, Morena Baccarin, T.J. Miller',
+        'plot': `This is the origin story of former Special Forces operative turned mercenary Wade Wilson, 
+                who after being subjected to a rogue experiment that leaves him with accelerated healing powers, 
+                adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, 
+                Deadpool hunts down the man who nearly destroyed his life.`,
+    },
 ]
 
 const Gallery = () => {
@@ -18,13 +36,25 @@ const Gallery = () => {
             <h4 className='px-2 p-3 mt-5'>Currently Showing</h4>
             <div className='grid grid-cols-6 border-t-2 border-pearl-aqua'>
                 {/* Movie panels for currently showing movies go here */}
-                <MoviePanel />
+                {/* Set the button to go to the correct place book now/pre-book */}
+                {
+                    jsonDataArray.map((jsonObj) => <MoviePanel key={jsonObj.id} 
+                                                                    jsonObj={jsonObj}
+                                                                    buttonSettings=
+                                                                        {{'link':'#','text':'Book Now!'}}
+                                                                    />)
+                }
             </div>
-            <div className='py-3 border-b-2 border-pearl-aqua'></div>
-            <h4 className='px-4 py-3 mt-5'>Coming soon!</h4>
-            <div>
+            <h4 className='px-2 py-3 mt-5'>Coming soon!</h4>
+            <div className='grid grid-cols-6 border-t-2 border-pearl-aqua'>            
                 {/* More movie panels for upcoming movies here */}
-
+                {
+                    jsonDataArray.map((jsonObj) => <MoviePanel key= {jsonObj.id} 
+                                                                    jsonObj={jsonObj}
+                                                                    buttonSettings=
+                                                                        {{'link':'#', 'text':'Pre-book'}}    
+                                                                    />)
+                }
             </div>
         </div>
     );
--- qa-cinema/src/Components/MoviePanel.jsx
+++ qa-cinema/src/Components/MoviePanel.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MoviePanel = () => {
+const MoviePanel = ({jsonObj, buttonSettings}) => {
+
+    const {imgSrc, title, rating, year, length, actors, plot} = jsonObj;
 
     return (
         //  Create and return a panel for one movie
@@ -8,22 +11,22 @@ const MoviePanel = () => {
             {/* Transform the image div to scale up poster on hover */}
             <div className='pt-3 col-start-1 col-span-1 hover:scale-150'>
                 <img 
-                src='https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
-                alt='movie poster'></img>
+                src={imgSrc}
+                alt={`movie poster for ${title}`}></img>
             </div>
             <div className='col-start-2 col-end-6 ...'>
                 <div className='py-8 ml-6'>
                     <article>
-                        <h1 className='text-4xl pb-2'>Avengers Assemble</h1>
+                        <h1 className='text-4xl pb-2'>{title}</h1>
                         {/* TODO: make classification a link? */}
-                        <ul className='text-sm pb-2'><li className='inline pr-1'>2012</li><li className='inline pr-1'><a href='#'>12A</a></li><li className='inline pr-1'>2h 23m</li></ul>
-                        {/* <p className='text-xs pb-2'><span className='pe-2'>2012</span><span className='pe-2'>12A</span><span>2h 23m</span></p> */}
-                        <h2 className='pb-3'>Starring - Robert Downey Jnr, Chris Evans</h2>
-                        <p className='text-sm pb-5'>Earth's mightiest heroes must come together and learn to fight 
-                            as a team if they are going to stop the 
-                            mischievous Loki and his alien army from enslaving humanity.</p>
+                        <ul className='text-sm pb-2'><li className='inline pr-1'>{year}</li><li className='inline pr-1'>
+                                <a href='#'>{rating}</a></li><li className='inline pr-1'>{length} mins</li></ul>
+                        <h2 className='pb-3'>{actors}</h2>
+                        <p className='text-sm pb-5'>{plot}</p>
                     </article>
-                    <button className='custom-button'>Book now!</button>
+                    <Link to={buttonSettings.link}>
+                        <button className='custom-button'>{buttonSettings.text}</button>
+                    </Link>
                 </div>
             </div>
         </>

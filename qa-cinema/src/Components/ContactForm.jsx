700c1d19e50f72312a3eb8f0955dbf2678c3a0ca
Helen Pickavance <helenpjobs@gmail.com>
2023-05-12 19:49:42 +0100

    EmailJS issues fixed

--- qa-cinema/src/Components/ContactForm.jsx
+++ qa-cinema/src/Components/ContactForm.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useState } from "react";
 import emailjs from '@emailjs/browser';
+import React, { useState } from "react";
 
 const ContactForm = () => {
     const emailServiceID = 'service_01gk0xb';
@@ -8,9 +7,10 @@ const ContactForm = () => {
     const emailPublicKey = 'iZfkvRJeWhM7gWGWL';
 
     const [formData, setFormData] = useState({
-                                                email: "",
-                                                subject: "",
-                                                message: "",
+                                                fName: '',
+                                                email: '',
+                                                subject: '',
+                                                message: '',
                                             });
 
     const handleChange = (event) => {
@@ -22,35 +22,29 @@ const ContactForm = () => {
     const handleSubmit = (event) => {
         // React - prevent default DOM behaviour, to implement our own
         event.preventDefault();
+
+        const {fName, email, subject, message} = formData;
+        let comboMessage = `Customer subject: - ${subject} \n Message: ${message}`;
         
         // TODO: fix the regex for emails
-        let regex = new RegExp('/[^\s@]+@[^\s@]+\.[^\s@]+/');
-        const {email, subject, message} = formData;
+        // let regex = new RegExp("/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm");
+        
         // if((!email) || (!regex.test(email))) {
         //     alert('invalid email');
         //     return;
         // }
         
-        // Parameters that match our EmailJS email template
-        // TODO - add subject to template?
-        const emailTemplateParams = {
-            to_name: 'QA Cinema',
-            from_name: 'Barry',
-            message: 'From app',
-            reply_to: 'ellohez@gmail.com',
-        }
-        
-        emailjs.sendForm(emailServiceID, emailTemplateID, emailPublicKey, emailTemplateParams)
+        // emailjs.send(emailServiceID, emailTemplateID, emailTemplateParams, emailPublicKey)
+        emailjs.send(emailServiceID, emailTemplateID, {'to_name': 'QA Cinema',
+                                                        'from_name': fName,
+                                                        'message': comboMessage,
+                                                        'reply_to': email}, emailPublicKey)
         .then((result) => {
             alert("Message Sent, We will get back to you shortly", result.text);
         },
         (error) => {
             alert("An error occurred, Please try again", error.text);
         });
-
-        // alert(`Email: ${email},
-        //     Subject: ${subject}, 
-        //     Message: ${message}`);
     }
 
     return (
@@ -59,9 +53,24 @@ const ContactForm = () => {
         <h1 className="custom-header">Please contact us!</h1>
             <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
                 onSubmit={handleSubmit}>
-                <div className="flex-1">                 
-                    <label  
-                    className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                <div className="flex-1">
+                    <label
+                        className="block tracking-wide text-white text-xs font-bold mb-2"
+                        htmlFor="fName">
+                            Name
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="fName"
+                        name="fName"
+                        placeholder="Name - required"
+                        type="text"
+                        required
+                        value={formData.fName}
+                        onChange={handleChange}   
+                    />             
+                    <label
+                    className="block tracking-wide text-white text-xs font-bold mb-2"
                     htmlFor="email">
                         Email
                     </label>
@@ -82,7 +91,7 @@ const ContactForm = () => {
                 </div>
                 <div className="flex-1">
                     <label
-                     className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                     className="block tracking-wide text-white text-xs font-bold mb-2"
                      htmlFor="subject">
                         Subject
                     </label>
@@ -99,7 +108,7 @@ const ContactForm = () => {
                 </div>
 
                 <div className="flex-1">
-                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                    <label className="block tracking-wide text-white text-xs font-bold mb-2"
                     htmlFor="message">
                         Message
                     </label>

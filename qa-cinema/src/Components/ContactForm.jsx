92ab18d1be260ccf1bace58514dd577c93283acf
Helen Pickavance <helenpjobs@gmail.com>
2023-05-13 11:22:54 +0100

    Styling better

--- qa-cinema/src/Components/ContactForm.jsx
+++ qa-cinema/src/Components/ContactForm.jsx
@@ -1,5 +1,5 @@
 import emailjs from '@emailjs/browser';
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const ContactForm = () => {
     const emailServiceID = 'service_01gk0xb';
@@ -27,7 +27,7 @@ const ContactForm = () => {
         let comboMessage = `Customer subject: - ${subject} \n Message: ${message}`;
         
         // TODO: fix the regex for emails
-        // let regex = new RegExp("/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm");
+        // let regex = new RegExp('/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm');
         
         // if((!email) || (!regex.test(email))) {
         //     alert('invalid email');
@@ -40,95 +40,96 @@ const ContactForm = () => {
                                                         'message': comboMessage,
                                                         'reply_to': email}, emailPublicKey)
         .then((result) => {
-            alert("Message Sent, We will get back to you shortly", result.text);
+            alert('Message Sent, We will get back to you shortly', result.text);
         },
         (error) => {
-            alert("An error occurred, Please try again", error.text);
+            alert('An error occurred, Please try again', error.text);
         });
     }
 
     return (
         <>
-        <div className="items-center justify-between">
-        <h1 className="custom-header">Please contact us!</h1>
-            <form className="shadow-md rounded font-sans text-sm px-8 pt-6 pb-8 mb-4"
+        <div className='content-center'>
+        <h1 className='custom-header'>Please contact us!</h1>
+            <form className='shadow-md place-items-center rounded 
+                                    font-sans text-sm px-8 pt-6 pb-8 mt-4 mb-6'
                 onSubmit={handleSubmit}>
-                <div className="flex-1 mx-3">
+                <div className='flex-auto align-middle w-3/5'>
                     <label
-                        className="custom-label"
-                        htmlFor="fName">
+                        className='custom-label'
+                        htmlFor='fName'>
                             Name
                     </label>
                     <input
                         className='required-input'
-                        id="fName"
-                        name="fName"
-                        placeholder="Name - required"
-                        type="text"
+                        id='fName'
+                        name='fName'
+                        placeholder='Name - required'
+                        type='text'
                         required
                         value={formData.fName}
                         onChange={handleChange}   
                     />             
                     <label className='custom-label'
-                    htmlFor="email">
+                    htmlFor='email'>
                         Email
                     </label>
                     <input 
-                    className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="email"
-                        // aria-describedby="emailHelp"
-                        name="email"
-                        placeholder="Enter email"
-                        type="email"
+                    className='required-input'
+                        id='email'
+                        // aria-describedby='emailHelp'
+                        name='email'
+                        placeholder='Enter email'
+                        type='email'
                         required
                         value={formData.email}
                         onChange={handleChange}
                     />
                     {/* <p id='email-warning' className={ (validEmail ? 'hidden-invalid-p' : 'visible-invalid-p') }> */}
                         {/* Please input a valid email.</p> */}
-                    {/* <small id="emailHelp" class="form-text text-muted">Please enter your email (required).</small> */}
-                </div>
-                <div className="flex-1 mx-3">
+                    {/* <small id='emailHelp' class='form-text text-muted'>Please enter your email (required).</small> */}
+                {/* </div> */}
+                {/* <div className='flex-1 mx-3'> */}
                     <label
-                     className="custom-label"
-                     htmlFor="subject">
+                     className='custom-label'
+                     htmlFor='subject'>
                         Subject
                     </label>
                     <input
-                        className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="subject"
-                        name="subject"
-                        placeholder="Subject - required"
-                        type="text"
+                        className='required-input'
+                        id='subject'
+                        name='subject'
+                        placeholder='Subject - required'
+                        type='text'
                         required
                         value={formData.subject}
                         onChange={handleChange}
                     />
-                </div>
+                {/* </div> */}
 
-                <div className="flex-1">
-                    <label className="custom-label"
-                    htmlFor="message">
+                {/* <div className='flex-1'> */}
+                    <label className='custom-label'
+                    htmlFor='message'>
                         Message
                     </label>
                     <input
-                        className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="message"
-                        name="message"
-                        type="textarea"
+                        className='required-input'
+                        id='message'
+                        name='message'
+                        type='textarea'
                         required
                         value={formData.message}
                         onChange={handleChange}
                     />
-                </div>
-                <div className="flex-1">
+                {/* </div> */}
+                {/* <div className='flex-1'> */}
                     <button 
-                    className="custom-button"
-                    type="submit">
+                    className='custom-button'
+                    type='submit'>
                         Submit
                     </button>
                 </div>
-        </form>
+            </form>
         </div>
         </>
     )

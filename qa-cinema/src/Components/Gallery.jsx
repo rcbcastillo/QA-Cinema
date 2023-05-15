42ded42a600e99f209e195dd4f76338e03dad0ee
Helen Pickavance <helenpjobs@gmail.com>
2023-05-15 19:26:00 +0100

    Movie fix - DB data rendering

--- qa-cinema/src/Components/Gallery.jsx
+++ qa-cinema/src/Components/Gallery.jsx
@@ -1,65 +1,79 @@
-import React from 'react';
-import MoviePanel from './MoviePanel';
+import React, { useState, useEffect } from "react";
+import * as api from "../api";
+import MoviePanel from "./MoviePanel";
 
 const jsonDataArray = [
-    {
-        'id': '12223',
-        'imgSrc': 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-        'title': 'Avengers Assemble',
-        'rating': '12A',
-        'year': 2012,
-        'length': 143,
-        'actors': 'Robert Downey Jnr, Chris Evans',
-        'plot': `Can the world's most powerful team of Super Heroes put aside their differences and learn to 
+  {
+    id: "12223",
+    imgSrc:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    title: "Avengers Assemble",
+    rating: "12A",
+    year: 2012,
+    length: 143,
+    actors: "Robert Downey Jnr, Chris Evans",
+    plot: `Can the world's most powerful team of Super Heroes put aside their differences and learn to 
                 live together under one roof long enough to save the world from ultimate destruction? The answer 
-                will be revealed when the Red Skull and M.O.D.O.K. wage war on The Avengers.`
-    },
-    {
-        'id': '22234',
-        'imgSrc': 'https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-        'title': 'Deadpool',
-        'rating': '15',
-        'year': '2016',
-        'length': 108,
-        'actors': 'Ryan Reynolds, Morena Baccarin, T.J. Miller',
-        'plot': `This is the origin story of former Special Forces operative turned mercenary Wade Wilson, 
+                will be revealed when the Red Skull and M.O.D.O.K. wage war on The Avengers.`,
+  },
+  {
+    id: "22234",
+    imgSrc:
+      "https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    title: "Deadpool",
+    rating: "15",
+    year: "2016",
+    length: 108,
+    actors: "Ryan Reynolds, Morena Baccarin, T.J. Miller",
+    plot: `This is the origin story of former Special Forces operative turned mercenary Wade Wilson, 
                 who after being subjected to a rogue experiment that leaves him with accelerated healing powers, 
                 adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, 
                 Deadpool hunts down the man who nearly destroyed his life.`,
-    },
-]
+  },
+];
 
 const Gallery = () => {
+  const [movies, setMovies] = useState([]);
 
-    // TODO: Remove Link - just set button text and useContext in BookingController
-    // to move to the TicketOffice component
+  useEffect(() => {
+    api.getMovies().then((response) => {
+      setMovies(response);
+    });
+  }, []);
+
+  // TODO: Remove Link - just set button text and useContext in BookingController
+  // to move to the TicketOffice component
+  if (movies) {
     return (
-        <div className='px-4 mt-2'>
-            <h1 className='custom-header'>Currently Showing</h1>
-            <div className='outer-panel-div'>
-                {/* Movie panels for currently showing movies go here */}
-                {/* Set the button to go to the correct place book now/pre-book */}
-                {
-                    jsonDataArray.map((jsonObj) => <MoviePanel key={jsonObj.id} 
-                                                                    jsonObj={jsonObj}
-                                                                    buttonSettings=
-                                                                        {{'link':'#','text':'Book Now!'}}
-                                                                    />)
-                }
-            </div>
-            <h2 className='custom-header'>Coming soon!</h2>
-            <div className='outer-panel-div'>            
-                {/* More movie panels for upcoming movies here */}
-                {
-                    jsonDataArray.map((jsonObj) => <MoviePanel key= {jsonObj.id} 
-                                                                    jsonObj={jsonObj}
-                                                                    buttonSettings=
-                                                                        {{'link':'#', 'text':'Pre-book'}}    
-                                                                    />)
-                }
-            </div>
+    
+      <div className="px-4 mt-2">
+        <h1 className="custom-header">Currently Showing</h1>
+        <div className="outer-panel-div">
+          {/* Movie panels for currently showing movies go here */}
+          {/* Set the button to go to the correct place book now/pre-book */}
+          {movies.map((movie, index) => (
+            <MoviePanel
+              key={index}
+              movie={JSON.parse(JSON.stringify(movie))}
+              //movie={{...movie}}
+              buttonSettings={{ link: "#", text: "Book Now!" }}
+            />
+          ))}
         </div>
+        <h2 className="custom-header">Coming soon!</h2>
+        <div className="outer-panel-div">
+          {/* More movie panels for upcoming movies here */}
+          {/* {jsonDataArray.map((jsonObj) => (
+            <MoviePanel
+              key={jsonObj.id}
+              jsonObj={jsonObj}
+              buttonSettings={{ link: "#", text: "Pre-book" }}
+            />
+          ))} */}
+        </div>
+      </div>
     );
-}
+  }
+};
 
 export default Gallery;
--- qa-cinema/src/Components/MoviePanel.jsx
+++ qa-cinema/src/Components/MoviePanel.jsx
@@ -2,34 +2,35 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FilmContext } from './BookingController';
 
-const MoviePanel = ({jsonObj, buttonSettings}) => {
+const MoviePanel = ({movie, buttonSettings}) => {
+    const releaseDate = new Date(movie.Released);
 
     const { chosenFilmId, setChosenFilmId } = useContext(FilmContext);
-    const { id, imgSrc, title, rating, year, length, actors, plot } = jsonObj;
 
     return (
         //  Create and return a panel for one movie
         <>
             {/* Transform the image div to scale up poster on hover */}
+            {/* console.log({movie}); */}
             <div className='inner-panel-img-div'>
                 <img 
-                src={imgSrc}
-                alt={`movie poster for ${title}`}></img>
+                src={movie.Poster}
+                alt={`movie poster for ${movie.Title}`}></img>
             </div>
             {/* Note: '...' class doesn't seem to work in index.css 
                 so it's not been extracted as custom class*/}
             <div className='col-start-2 col-end-6 ...'>
                 <div className='py-8 ml-6'>
                     <article>
-                        <h1 className='text-4xl pb-2'>{title}</h1>
+                        <h1 className='text-4xl pb-2'>{movie.Title}</h1>
                         {/* Rating <li> links to classification page */}
-                        <ul className='text-sm pb-2'><li className='inline pr-1'>{year}</li><li className='inline pr-1'>
-                            <Link to='/classifications'>{rating}</Link></li><li className='inline pr-1'>{length} mins</li></ul>
-                        <h2 className='pb-3'>{actors}</h2>
-                        <p className='text-sm pb-5'>{plot}</p>
+                        <ul className='text-sm pb-2'><li className='inline pr-1'>{releaseDate.getFullYear()}</li><li className='inline pr-1'>
+                            <Link to='/classifications'>{movie.Rated}</Link></li><li className='inline pr-1'>{movie.Runtime}</li></ul>
+                        <h2 className='pb-3'>{movie.Actors}</h2>
+                        <p className='text-sm pb-5'>{movie.Plot}</p>
                     </article>
                     <Link to={buttonSettings.link}>
-                        <button className='custom-button' onClick={() => setChosenFilmId(id)}>
+                        <button className='custom-button' onClick={() => setChosenFilmId(movie._id)}>
                                     {buttonSettings.text}</button>
                     </Link>
                 </div>

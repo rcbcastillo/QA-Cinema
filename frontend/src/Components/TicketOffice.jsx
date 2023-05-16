203040ed71807f182541fd422c3734953585ac45
Helen Pickavance <helenpjobs@gmail.com>
2023-05-16 15:20:00 +0100

    State for ticket controls

--- frontend/src/Components/TicketOffice.jsx
+++ frontend/src/Components/TicketOffice.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { MovieContext } from "./BookingController";
 
 const TicketOffice = () => {
@@ -6,39 +6,96 @@ const TicketOffice = () => {
   const { chosenMovie, setChosenMovie } = useContext(MovieContext);
   const screenDate = new Date(chosenMovie.ScreenDateTime);
 
+  const [formData, setFormData] = useState({
+    adults: 0,
+    children: 0,
+    concessions: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { adults, children, concessions } = formData;
+    console.log(`Adult tickets: ${adults} Child tickets: ${children} Concession Tickets: ${concessions}`);
+  };
+
   // Show the screen, date and time and allow the user to select
   // number of tickets by type.
   return (
     <div className="w-full h-full">
       <div className="flex p-4 m-8">
-        <h3>{chosenMovie.Title}</h3>
+        <h3 className="custom-header">{chosenMovie.Title}</h3>
         <div className="">
-          {/* Output the screen, time and date */}
-          <h6>Screen: {chosenMovie.ScreenNum}</h6>
-          <h6>Date: {screenDate.toDateString()}</h6>
-          <h6>Time: {screenDate.toTimeString()}</h6>
+          <article>
+            {/* Output the screen, time and date */}
+            <h6>Screen: {chosenMovie.ScreenNum}</h6>
+            <h6>Date: {screenDate.toDateString()}</h6>
+            <h6>Time: {screenDate.toTimeString()}</h6>
+          </article>
         </div>
       </div>
 
-      {/* TODO: Add prices */}
-      <div className="flex p-4 m-8">
-        <label className="">
-          Adult tickets - £3
-          <input className="ml-2" type="number" min="1"></input>
-        </label>
-        <label className="">
-          Child tickets - £
-          <input className="ml-2" type="number" min="1"></input>
-        </label>
-        <label className="">
-          Concession tickets - £
-          <input className="ml-2" type="number" min="1"></input>
-        </label>
-      </div>
-
-      <button className="border-2 mb-11" onClick={() => setChosenMovie(null)}>
-        Cancel
-      </button>
+      <form
+        className="shadow-md place-items-center rounded"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex p-2">
+          <label className="">
+            Adult tickets - £3.00
+            <input
+              className="ml-2 text-black rounded"
+              type="number"
+              id="adults"
+              name="adults"
+              placeholder="0"
+              value={formData.adults}
+              min="0"
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="">
+            Child tickets - £2.00
+            <input
+              className="ml-2 text-black rounded"
+              type="number"
+              id="children"
+              name="children"
+              placeholder="0"
+              value={formData.children}
+              min="0"
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="">
+            Concession tickets - £1.00
+            <input
+              className="ml-2t text-black rounded"
+              type="number"
+              id="concessions"
+              name="concessions"
+              placeholder="0"
+              value={formData.concessions}
+              min="0"
+              onChange={handleChange}
+            ></input>
+          </label>
+        </div>
+        {/* <button className="border-2 mb-11"> */}
+        <button className="sm-custom-button mb-11" type="submit">
+          Proceed to payment
+        </button>
+      </form>
+      <button
+          className="sm-custom-button mb-11"
+          // type="reset"
+          onClick={() => setChosenMovie(null)}>
+            Cancel
+        </button>
     </div>
   );
 };

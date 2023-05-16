29012c580b242a45ab3dfcb34cfae5e0350913c7
44jovi <kwong.jovi@gmail.com>
2023-05-16 10:32:57 +0100

    progress(app, Booking)

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const movieRouter = require("./routes/movieRoutes");
 const userRouter = require("./routes/userRoutes");
 const commentRouter = require("./routes/commentRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
+const { stripeKey } = require("./resources/stripe_api_key");
+const stripe = require("stripe")(stripeKey);
 
 app.use(express.json());
 
@@ -15,10 +17,15 @@ app.use("/users", userRouter);
 app.use("/comments", commentRouter);
 app.use("/bookings", bookingRouter);
 
+// Stripe API
+// TODO: create router for Stripe API requests?
+
 // Error handling
 app.use((err, req, res, next) => {
   res.status(err.status).send(err.msg);
 });
 
-const server = app.listen(9090);
+const server = app.listen(9090, () =>
+  console.log("running and listening on port 9090")
+);
 module.exports = server;

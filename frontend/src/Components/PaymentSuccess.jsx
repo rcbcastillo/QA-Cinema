6ea5dc9ad7589fed8e58f579c3eb3be5d8b3c4dc
Helen Pickavance <helenpjobs@gmail.com>
2023-05-17 13:15:05 +0100

    PaymentSuccess component added

---
+++ frontend/src/Components/PaymentSuccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaymentSuccess = () => {
+  return (
+    <div>
+      <h1 className='custom-header'>Thank you for booking tickets!</h1>
+    </div>
+  )
+}
+
+export default PaymentSuccess

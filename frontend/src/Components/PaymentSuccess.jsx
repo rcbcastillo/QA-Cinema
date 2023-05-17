08c343a642cc409441c051e61e7dce9ba87014ed
Helen Pickavance <helenpjobs@gmail.com>
2023-05-17 14:24:18 +0100

    Query param fetch logic

--- frontend/src/Components/PaymentSuccess.jsx
+++ frontend/src/Components/PaymentSuccess.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const PaymentSuccess = () => {
-  return (
-    <div>
-      <h1 className='custom-header'>Thank you for booking tickets!</h1>
-    </div>
-  )
+    
+    const queryParameters = new URLSearchParams(window.location.search);
+    let sessionId = queryParameters.get("session_id");
+
+    return (
+      <div>
+        <h1 className='custom-header'>Thank you for booking tickets!</h1>
+        <p>Session ID: {sessionId}</p>
+      </div>
+    )
 }
 
 export default PaymentSuccess

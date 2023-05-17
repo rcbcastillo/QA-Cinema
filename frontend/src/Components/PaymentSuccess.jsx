1df27e18b605e051ea2ef3b836f02b402b8049b4
Helen Pickavance <helenpjobs@gmail.com>
2023-05-17 16:12:59 +0100

    PaymentSuccess DB communication

--- frontend/src/Components/PaymentSuccess.jsx
+++ frontend/src/Components/PaymentSuccess.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const PaymentSuccess = () => {
-    
-    const queryParameters = new URLSearchParams(window.location.search);
-    let sessionId = queryParameters.get("session_id");
+  const queryParameters = new URLSearchParams(window.location.search);
+  const sessionId = "6464e0178169730f0f511776";
+  // const sessionId = queryParameters.get("session_id");
+  const [bookingRes, setBookingRes] = useState(null);
 
-    return (
-      <div>
-        <h1 className='custom-header'>Thank you for booking tickets!</h1>
+  const uri = `http://localhost:9090/bookings/${sessionId}`;
+  console.log(uri);
+
+  useEffect(() => {
+    axios
+      .get(uri)
+      .then((response) => {
+        setBookingRes(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [bookingRes]);
+
+  return (
+    <>
+      <h1 className="custom-header">You have booked!</h1>
+      <h3>Film: {bookingRes.movieTitle}</h3>
+      <div className="flex align-middle pb-4 mb-5">
         <p>Session ID: {sessionId}</p>
       </div>
-    )
-}
+    </>
+  );
+};
 
-export default PaymentSuccess
+export default PaymentSuccess;

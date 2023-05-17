d046713679eec2e42258391f6c854a56157a522f
rcbcastillo <rcbcastillo@gmail.com>
2023-05-17 13:25:48 +0100

    Add the CommentPanel component to get comments by movieId

--- frontend/src/Components/CommentPanel.jsx
+++ frontend/src/Components/CommentPanel.jsx
@@ -1,50 +1,47 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as api from "../api";
-import { FilmContext } from "./BookingController";
-import PrintTableRows from "./PrintTableRows";
+import CommentsList from "./CommentsList";
 
 const CommentPanel = ({ movie }) => {
   const [comments, setComments] = useState([]);
-  //const { chosenFilmId, setChosenFilmId } = useContext(FilmContext);
+  const [showComments, setShowComments] = useState(false);
 
   useEffect(() => {
-    api.getComments().then((comments) => setComments(comments));
+    api
+      .getCommentsByMovieId(movie._id)
+      .then((response) => setComments(response));
   }, []);
 
-  return (
-    //  Create and return a panel for one movie
-    <>
-      {/* Transform the image div to scale up poster on hover */}
-      <div className="inner-panel-img-div">
-        <img src={movie.Poster} alt={`movie poster for ${movie.Title}`}></img>
-      </div>
-      {/* Note: '...' class doesn't seem to work in index.css 
+  if (comments) {
+    return (
+      //  Create and return a panel for one movie
+      <>
+        <div className="inner-panel-img-div-without-hovering">
+          <p className="text-center">{movie.Title}</p>
+          <img src={movie.Poster} alt={`movie poster for ${movie.Title}`}></img>
+        </div>
+        {/* Note: '...' class doesn't seem to work in index.css 
                 so it's not been extracted as custom class*/}
-      <div className="col-start-2 col-end-6 ...">
-        <div className="py-8 ml-6">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {comments.map((comment) => (
-                <PrintTableRows rowData={{ ...comment }} />
-              ))}
-            </tbody>
-          </table>
+        <div className="col-start-2 col-end-6 ...">
+          <div className="py-8 ml-6">
+            <div className="comments-onclick">
+              <button onClick={() => setShowComments((current) => !current)}>
+                {showComments ? "Hide comments" : "See comments"}
+              </button>
+              {showComments ? <CommentsList comments={comments} /> : null}
+            </div>
+          </div>
 
           <button
             className="custom-button"
-            //onClick={() => setChosenFilmId(movie._id)}
+            //onClick={() => setChosenMovie(movie)}
           >
             Post comment
           </button>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default CommentPanel;

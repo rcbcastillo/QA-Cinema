c86f749c0ea5de927101afbcaa6cbb14a212a03f
Helen Pickavance <helenpjobs@gmail.com>
2023-05-17 16:41:39 +0100

    Payment DB working

--- frontend/src/Components/PaymentSuccess.jsx
+++ frontend/src/Components/PaymentSuccess.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const PaymentSuccess = () => {
   const queryParameters = new URLSearchParams(window.location.search);
-  const sessionId = "6464e0178169730f0f511776";
+  const sessionId = "6464e6236912fcb920062b8a";
   // const sessionId = queryParameters.get("session_id");
   const [bookingRes, setBookingRes] = useState(null);
 
@@ -20,17 +20,37 @@ const PaymentSuccess = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, [bookingRes]);
+  }, [uri]);
 
-  return (
-    <>
-      <h1 className="custom-header">You have booked!</h1>
-      <h3>Film: {bookingRes.movieTitle}</h3>
-      <div className="flex align-middle pb-4 mb-5">
-        <p>Session ID: {sessionId}</p>
-      </div>
-    </>
-  );
+  if (bookingRes) {
+    return (
+      <>
+        <div>
+          <h1 className="custom-header">You are going to the cinema!</h1>
+          <strong>Session ID: {sessionId}</strong>
+          <div className="flex justify-center pb-4 mb-5">
+          <h3>Film: {bookingRes.movieTitle}</h3>
+          <table>
+            <thead>
+              <tr>
+                {/* <td>Sreening Date/Time</td> */}
+                <td>Seats Booked</td>
+                <td>Adults</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {/* <td>{Date(bookingRes.screeningDateTime.toString()).toDateString()}</td> */}
+                <td>{bookingRes.seatsBooked}</td>
+                <td>{bookingRes.adult}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default PaymentSuccess;

0f5d91766ed4f48ce0189750c912665265f51df7
44jovi <kwong.jovi@gmail.com>
2023-05-18 12:03:41 +0100

    feature(About): add team info to about page

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -1,29 +1,25 @@
+import React, { useState } from "react";
+import teamData from "./team-data.json";
+import JsonTable from "./JsonTable";
+
 const About = () => {
-    return(
-        <>
-        <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  const [jsonData] = useState(teamData);
+
+  return (
+    <>
+      {/* <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4"> */}
+      <div style={{ padding: "10px", margin: "auto", width: "50%" }}>
         <div className="about-box">
-            <div className="about-heading">
-            <h2>About Page</h2>
-            <p>QA Cinema is part of QA - QA delivers training, apprenticeships and tech skills to UK companies large and small. We believe everything starts with our learners – transforming their careers, and their lives, is what motivates us.</p>
-            </div>
+          <div className="about-heading">
+            <h1 className="custom-header">QA Cinema by Sky Oyster</h1>
+            <h1 className="custom-header">Meet the team</h1>
+          </div>
         </div>
         <div className="about-content">
-            <p>ABOUT QA CINEMA
-
-            QA Cinema is a leader in the premium entertainment cinema sector in the UK.
-
-            Committed to continued growth and development of its premium entertainment ‘big screen’ experience, QA Cinema delivers the best choice of content, combined with the best technology in the best environment.
-
-            QA Cinema has 91 state of the art cinemas throughout the UK and Ireland, with over 870 screens. This includes 271 3D screens, 11 Extreme Screens, 7 Gold Class screens, 6 Scene Screens and Bars and 3 IMAX screens where QA Cinema project the majority of our films in Sony Digital Cinema 4K for ultra high definition pictures with four times more detail. With over 150,000 seats, 98% of which are stadium seating, QA Cinema's innovative development programme has led the way in the UK. QA Cinema Westfield London (February 2010) and QA Cinema Westfield Stratford City (September 2011) remain the first and third highest grossing cinemas in the country.
-            QA Cinemas around the UK offer a range of content, in addition to highly anticipated blockbusters, with event cinema titles including opera, ballet, musicals, national theatre, live streaming of sporting events and concerts through to hosting Gaming championships and dedicating multiple screens to eGaming arenas.
-
-            QA Cinema was founded in the UK following the acquisition of the Warner Village Cinemas in 2003 and is part of the largest cinema group in Europe, QA Cinema International.
-
-            Over the years QA Cinema has won numerous accolades for its service and innovations, including more recently International Exhibitor of the Year at CineEurope 2014 and Cinema Exhibitor of the Year Screen, Marketing & Distribution Awards 2013.</p>
+          <JsonTable jsonData={jsonData} />
         </div>
-        </div>
-        </>
-    )
-}
-export default About
+      </div>
+    </>
+  );
+};
+export default About;

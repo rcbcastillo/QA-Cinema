f9d98c50fa633197ef5f0b47ffe6b3bbe10fcc74
Helen Pickavance <helenpjobs@gmail.com>
2023-05-18 12:32:24 +0100

    Hooked 'Book' to films in navbar link

--- frontend/src/Components/NavbarComponent.jsx
+++ frontend/src/Components/NavbarComponent.jsx
@@ -141,7 +141,7 @@ function NavList() {
       </Typography>
       <Typography
         as="a"
-        href="/booking"
+        href="/films"
         variant="small"
         
         className="font-normal"
@@ -163,22 +163,7 @@ function NavList() {
           Forums
         </ListItem>
       </Typography>
-      <ListItem className="relative flex w-full gap-2 md:w-max">
-      <div className="relative flex w-full gap-2 md:w-max">
-          <Input
-            
-            type="search"
-            label="Type here..."
-            className="pr-20"
-            containerProps={{
-              className: "min-w-[288px]",
-            }}
-          />
-          <Button size="sm" className=" rounded">
-            Search
-          </Button>
-        </div>
-      </ListItem>
+      
     </List>
   );
 }
